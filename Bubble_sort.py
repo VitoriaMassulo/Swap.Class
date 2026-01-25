d2d376aae50f1f3143d3e2b9caaaada003d04d82
VitoriaMassulo <vitoriamassulo@icloud.com>
2026-01-25 22:01:40 +0200

    Update Bubble_sort.py
    Imagine a row of people of different heights. You want to organize them from shortest to tallest.
    1. You look at the first two people. If the one on the left is taller, you swap them.
    2. You move to the next pair (2nd and 3rd person) and do the same.
    3. By the time you reach the end of the row, the tallest person has "bubbled up" to the very end.
    4. You repeat this process for the rest of the people until everyone is in order.

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -1,1 +1,28 @@
+#Advanced (Algorithms & Performance)
+
+def bubble_sort(numbers):
+    # 'n' is the total number of items in our list
+    n = len(numbers)
+    
+    # The Outer Loop: Controls how many times we walk through the list
+    for i in range(n):
+        # The Inner Loop: Compares neighbor elements
+        # 'n - i - 1' makes the loop shorter each time because 
+        # the largest numbers are already at the end!
+        for j in range(0, n - i - 1):
+            
+            # 1. Compare: Is the current number bigger than the next one?
+            if numbers[j] > numbers[j + 1]:
+                
+                # 2. Swap: If yes, they trade places!
+                # This is the "Pythonic Swap" we talked about.
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+                
+    return numbers
+
+# Testing the logic:
+data = [5, 1, 4, 2]
+sorted_data = bubble_sort(data)
+print(f"Sorted list: {sorted_data}")
+
 
