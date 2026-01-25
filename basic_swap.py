f55dcea3e9d0b86225e5d72b80e0c261bc0a2bee
VitoriaMassulo <vitoriamassulo@icloud.com>
2026-01-25 21:50:03 +0200

    Update basic_swap.py
    The Temp method is like having a Table (temp).You put the Blue Book on the table, take the Red Book into your left hand, and then pick up the Blue Book from the table with your right hand.

--- basic_swap.py
+++ basic_swap.py
@@ -1,1 +1,14 @@
+#1. The "Temporary Variable" Method (The Standard Way)
+
+a = int(input("Enter A: "))
+b = int(input("Enter B: "))
+
+print(f"Before: a = {a}, b = {b}")
+
+# The Swap Logic
+temp = a   # Step 1: Save 'a' in a temporary 'box'
+a = b      # Step 2: Put 'b' into 'a'
+b = temp   # Step 3: Put the saved value from 'temp' into 'b'
+
+print(f"After:  a = {a}, b = {b}")
 
